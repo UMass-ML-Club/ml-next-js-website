bf6bcd2eb64ec9a9bfebd1b2f64702f228a91415
Timmald-School <118050114+Timmald-School@users.noreply.github.com>
2024-01-12 14:21:32 -0500

    More Button Mobile Fix

--- src/components/widgets/Testimonial.tsx
+++ src/components/widgets/Testimonial.tsx
@@ -5,13 +5,14 @@ import HeaderWidget from '../common/HeaderWidget';
 
 const Testimonial = () => {
   const { header, testimonials } = testimonialData;
-
+//TODO: Grid with 2 diff width cols: cards and button, 90-10 ratio or whatever. Cards is a div that flex-wraps as it shrinks, and button vertically centers button
+  //Or,
   return (
     <section className="bg-umass-neutral-light dark:bg-umass-neutral-dark" id="testimonial">
       <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
         {header && <HeaderWidget header={header} titleClassname="text-2xl sm:text-3xl" />}
         <div className="flex flex-wrap justify-center">
-          <div className="items-center grid grid-cols-4 gap-3 dark:text-white sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4">
+          <div className="items-center grid grid-cols-1 gap-3 dark:text-white sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
             {testimonials.map(({ name, occupation, comment, image, icon: Icon, href }, index) => (
               <div
                 key={`item-testimonial-${index}`}
